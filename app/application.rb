10cd44b401754ce71a83c4447eadb621916a6afe
Silas Michael Knight <silasfelinus@gmail.com>
2021-04-17 23:46:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,17 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
- 
 
- 
+
+
     if Time.new.strftime("%k") >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
     end
- 
+
     resp.finish
   end
- 
+
 end
